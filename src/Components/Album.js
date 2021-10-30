dc1b603ba79039232f38ed9d55adc2ea46968af6
Bojidar Boradjiev <bojidarboradjiev@gmail.com>
2021-10-30 16:28:29 +0300

    adding bootstrap

--- src/Components/Album.js
+++ src/Components/Album.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { addFavorite } from "../reduxToolkit/sliceFavorites";
 import { useLocation } from "react-router-dom";
+import { Card, Button } from "react-bootstrap";
 
 function Album() {
   const location = useLocation();
@@ -9,14 +10,22 @@ function Album() {
   return (
     <div className="album">
       {location.state.map((el, i) => (
-        <div
+        <Card
+          style={{ width: "330px", height: "600px" }}
           key={location.state[i].id}
-          onClick={() => dispatch(addFavorite(location.state[i]))}
-          className="thumbnail"
         >
-          <img alt="alternative" src={el.thumbnailUrl} />
-          <p>{el.title}</p>
-        </div>
+          <Card.Img variant="top" src={el.thumbnailUrl} />
+          <Card.Body className="card-body d-flex flex-column justify-content-between">
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>{el.title}</Card.Text>
+            <Button
+              variant="primary"
+              onClick={() => dispatch(addFavorite(location.state[i]))}
+            >
+              Add to faforites
+            </Button>
+          </Card.Body>
+        </Card>
       ))}
     </div>
   );
--- src/Components/Favorites.js
+++ src/Components/Favorites.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { removeFavorite } from "../reduxToolkit/sliceFavorites";
+import { Card, Button } from "react-bootstrap";
 
 import { useSelector, useDispatch } from "react-redux";
 
@@ -10,17 +11,19 @@ function Favorites() {
   return (
     <div className="favorites">
       {favorites.map((el, i) => (
-        <div key={i} className="favorite_card">
-          <img alt="alternative" src={el.thumbnailUrl} />
-          <button
-            onClick={() => {
-              dispatch(removeFavorite(el.id));
-            }}
-          >
-            Delete
-          </button>
-          <p>{el.title}</p>
-        </div>
+        <Card style={{ width: "330px", height: "600px" }} key={favorites.id}>
+          <Card.Img variant="top" src={el.thumbnailUrl} />
+          <Card.Body className="card-body d-flex flex-column justify-content-between">
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>{el.title}</Card.Text>
+            <Button
+              variant="primary"
+              onClick={() => dispatch(removeFavorite(el.id))}
+            >
+              Remove from favorites
+            </Button>
+          </Card.Body>
+        </Card>
       ))}
     </div>
   );
